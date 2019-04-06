d84b80429d484d8d2f9f194643b5ab9df6ad83f8
George Hunt <georgejhunt@gmail.com>
2019-04-06 10:37:47 -0700

    can generate regional zips

---
+++ osm-vector/base/main.js
@@ -0,0 +1,197 @@
+// main.js for base -- regional OSM vector tiles
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
+import {fromLonLat,toLonLat} from 'ol/proj';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import MVT from 'ol/format/MVT';
+import stylefunction from 'ol-mapbox-style/stylefunction';
+import {defaults as defaultControls, ScaleLine} from 'ol/control.js';
+import GeoJSON from 'ol/format/GeoJSON';
+import {Style, Fill, Stroke, Circle, Text} from 'ol/style';
+window.$ = window.jQuery = require('jquery');
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+const typeahead = require('./assets/bootstrap-typeahead.min.js');
+import Geolocation from 'ol/Geolocation'
+
+var scaleLineControl = new ScaleLine();
+// initial values for on event variables to get through startup
+var zoom = 3;
+var show = 'min';
+var lat = -117;
+var lon = 37;
+
+const map = new Map({
+  target: 'map-container',
+  controls: defaultControls().extend([
+    scaleLineControl
+  ]),
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: './tileserver.php/ocean/{z}/{x}/{y}.png'
+      })
+    })
+  ],
+  view: new View({
+    center: fromLonLat([-71.06, 42.37]),
+    zoom: 2
+  })
+});
+
+const detail = new VectorTileLayer({
+   source: new VectorTileSource({
+      format: new MVT(),
+      url: `./tileserver.php/detail/{z}/{x}/{y}.pbf`,
+      minZoom: 0,
+      maxZoom: 14
+   }),
+   declutter: true,
+});
+fetch('./assets/style-cdn.json').then(function(response) {
+   response.json().then(function(glStyle) {
+     stylefunction(detail, glStyle,"openmaptiles");
+   });
+});
+map.addLayer(detail);
+
+const boxLayer =  new VectorLayer({
+   source: new VectorSource({
+     format: new GeoJSON(),
+     url: './bboxes.geojson'
+   }),
+   style: function(feature) {
+     var name = feature.get("name");
+     if (typeof show !== 'undefined' &&
+          show != null && name == show) {
+       return new Style({
+         fill: new Fill({
+           color: 'rgba(67, 163, 46, 0)'
+         }),
+         stroke: new Stroke({
+           color: 'rgba(67, 163, 46, 1)',
+           width: 2
+         })
+       })
+     } else {
+       return new Style({
+         fill: new Fill({
+           color: 'rgba(255,255,255,0)'
+         }),
+         stroke: new Stroke({
+           color: 'rgba(255,255,255,0)'
+         })
+       })
+     } 
+   } 
+})
+map.addLayer(boxLayer);    
+
+var unitsSelect = document.getElementById('units');
+function onChange() {
+  scaleLineControl.setUnits(unitsSelect.value);
+}
+unitsSelect.addEventListener('change', onChange);
+onChange();
+
+var info_overlay = 0;
+$( document ).ready(function() {
+   // typeahead has (window.jQuery) at the end of its definition
+   window.$ = window.jQuery = jQuery;  // needs definition globally
+
+   info_overlay = document.getElementById('info-overlay');
+   function update_overlay(evt){
+       var locTxt = "Lat: " + lat.toFixed(3) + " Lon: " + lon.toFixed(3); 
+       var tilex = long2tile(lon,zoom);
+       var tiley = lat2tile(lat,zoom);
+       var zoomInfo = ' Zoom: ' + zoom.toFixed(1);
+       locTxt += "   TileX: " + tilex + " TileY: " + tiley + zoomInfo; 
+       info_overlay.innerHTML = locTxt;
+   }
+   map.on("pointermove", function(evt) {
+      var coords = toLonLat(evt.coordinate);
+      lat = coords[1];
+      lon = coords[0];
+      update_overlay();
+   });
+   map.on("moveend", function() {
+      zoom = map.getView().getZoom(); 
+      update_overlay();
+   });
+
+   var selections = Array(50);
+   function go_there(item){
+       for (var i=0;i<selections.length;i++){
+          if (selections[i].geonameid == item.value){
+             var there = fromLonLat([selections[i].lon,selections[i].lat]);
+             map.getView().setCenter(there);
+             map.getView().setZoom(10);
+             console.log(selections[i].lon + ' ' + selections[i].lat);
+          }
+       }
+       $('#search').val('');
+    }
+
+   $(function() {
+      $('#search').typeahead({
+       onSelect: function(item) {
+          console.log(item);
+          go_there(item);
+      },
+      ajax: {
+         url: './searchapi.php?searchfor='+$('#search').val(),
+         method: 'get',
+         triggerLength: 1,
+         displayField: 'name',
+         valueField: "geonameid",
+         dataType: "json",
+         preProcess: function (data) {
+          if (data.success === false) {
+            // Hide the list, there was some error
+            return false;
+          }
+          // We good!
+          selections = [];
+          for (var i=0;i<data.length;i++) {
+            data[i].name = data[i].name + ' ' + data[i].country_code + ' pop: ' + data[i].population;
+            var choice = {geonameid:data[i].geonameid,lon:data[i].longitude,lat:data[i].latitude};
+            selections.push(choice);
+          } 
+          return data;
+          }
+      }, // ajax get cities with his prefix
+   }); // typeahead onSelect
+}); // end of search selection
+
+
+   // Functions to compute tiles from lat/lon
+   function long2tile(lon,zoom) {
+      return (Math.floor((lon+180)/360*Math.pow(2,zoom)));
+   }
+   function lat2tile(lat,zoom)  {
+      return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom)));
+   }
+   var config = {};
+   var resp = $.ajax({
+      type: 'GET',
+      async: true,
+      url: './init.json',
+      dataType: 'json'
+   })
+   .done(function( data ) {
+      config = data;
+      var coord = [parseFloat(config.center_lon),parseFloat(config.center_lat)];
+      console.log(coord + "");
+      var there = fromLonLat(coord);
+      map.getView().setCenter(there);
+      map.getView().setZoom(parseFloat(config["zoom"]));
+      show = config.region;
+   });
+
+}); // end of document ready
--- tools/update_regions.json.py
+++ tools/update_regions.json.py
@@ -6,25 +6,31 @@ import json
 import sqlite3
 
 MR_SSD = os.environ.get("MR_SSD",'/root/mapgen')
-REGION_INFO = os.path.join(MR_SSD,'resources','regions.json')
-
-MR_HARD_DISK = os.environ.get("MR_HARD_DISK",'/hd/mapgen')
+REGION_INFO = os.path.join(MR_SSD,'../resources','regions.json')
 
 outstr = ''
 region_list = []
 with open(REGION_INFO,'r') as region_fp:
-   data = json.loads(region_fp.read())
+   try:
+      data = json.loads(region_fp.read())
+   except:
+      print("json error reading regions.json")
+      sys.exit(1)
    for region in data['regions'].keys():
-      mbtile = os.path.join(MR_HARD_DISK,'output',region+'.mbtiles')
-      if not os.path.isfile(mbtile):
-         mbtile = os.path.join(MR_SSD,'output',region+'.mbtiles')
-      conn = sqlite3.connect(mbtile)
-      c = conn.cursor()
-      sql = 'select value from metadata where name = "filesize"'
+      mbtile = os.path.join(MR_SSD,'output/stage2/',region+'.mbtiles')
+      if region == 'world':
+         mbtile = os.environ.get('PLANET_MBTILES','')
+      if mbtile == '':
+         print('problem with planet mbtile')
+         sys.exit(1)
       try:
+         conn = sqlite3.connect(mbtile)
+         c = conn.cursor()
+         sql = 'select value from metadata where name = "filesize"'
          c.execute(sql)
       except:
-         print("ERROR")
+         print("ERROR dealing with region:%s"%region)
+         sys.exit(1)
       row = c.fetchone()
 	   #print(row[0])
       if row:
