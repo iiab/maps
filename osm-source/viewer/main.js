85c4ba66863f20e8e55e8b17b8e97c905abd6cdb
George Hunt <georgejhunt@gmail.com>
2020-02-08 21:25:53 -0800

    right click functions -- now need the action routines

--- osm-source/viewer/main.js
+++ osm-source/viewer/main.js
@@ -1,4 +1,6 @@
-
+// right click branch working towards adding points and data to maps
+var ContextMenu = require('./ol-contextmenu.js');
+var ol = require('ol');
 // temp.js for base -- regional OSM vector tiles
 import 'ol/ol.css';
 import Map from 'ol/Map';
@@ -335,3 +337,120 @@ $(function() {
    }); // typeahead onSelect
 }); // end of search selection
 
+
+////////////////     below is context menu stuff  ///////////////////////
+var contextmenu_items = [
+   {
+     text: 'Add Data',
+     icon: 'img/pin_drop.png',
+     callback: marker,
+   },
+  {
+    text: 'View Data',
+    classname: 'bold',
+    icon: 'img/center.png',
+    //callback: center,
+  },
+  {
+    text: 'Save As ..',
+    classname: 'bold',
+    icon: 'img/center.png',
+    //callback: center,
+  },
+  {
+    text: 'Save',
+    classname: 'bold',
+    icon: 'img/center.png',
+    //callback: center,
+  },
+/*    text: 'Some Actions',
+    icon: 'img/view_list.png',
+    items: [
+      {
+        text: 'Center map here',
+        icon: 'img/center.png',
+        callback: center,
+      },
+    ],
+  },
+*/
+ // '-', // this is a separator
+];
+var removeMarkerItem = {
+  text: 'Remove this Marker',
+  classname: 'marker',
+  callback: removeMarker,
+};
+
+var contextmenu = new ContextMenu({
+  width: 180,
+  items: contextmenu_items,
+});
+map.addControl(contextmenu);
+
+contextmenu.on('open', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel, function(ft, l) {
+    return ft;
+  });
+  if (feature && feature.get('type') === 'removable') {
+    contextmenu.clear();
+    removeMarkerItem.data = {
+      marker: feature,
+    };
+    contextmenu.push(removeMarkerItem);
+  } else {
+    contextmenu.clear();
+    contextmenu.extend(contextmenu_items);
+    //contextmenu.extend(contextmenu.getDefaultItems());
+  }
+});
+
+map.on('pointermove', function(e) {
+  var pixel = map.getEventPixel(e.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel);
+
+  if (e.dragging) return;
+
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
+
+// from https://github.com/DmitryBaranovskiy/raphael
+function elastic(t) {
+  return (
+    Math.pow(2, -10 * t) * Math.sin(((t - 0.075) * (2 * Math.PI)) / 0.3) + 1
+  );
+}
+
+function center(obj) {
+  view.animate({
+    duration: 700,
+    easing: elastic,
+    center: obj.coordinate,
+  });
+}
+
+function removeMarker(obj) {
+  vectorLayer.getSource().removeFeature(obj.data.marker);
+}
+
+function marker(obj) {
+  var coord4326 = ol.proj.transform(obj.coordinate, 'EPSG:3857', 'EPSG:4326'),
+    template = 'Coordinate is ({x} | {y})',
+    iconStyle = new ol.style.Style({
+      image: new ol.style.Icon({ scale: 0.6, src: 'img/pin_drop.png' }),
+      text: new ol.style.Text({
+        offsetY: 25,
+        text: ol.coordinate.format(coord4326, template, 2),
+        font: '15px Open Sans,sans-serif',
+        fill: new ol.style.Fill({ color: '#111' }),
+        stroke: new ol.style.Stroke({ color: '#eee', width: 2 }),
+      }),
+    }),
+    feature = new ol.Feature({
+      type: 'removable',
+      geometry: new ol.geom.Point(obj.coordinate),
+    });
+
+  feature.setStyle(iconStyle);
+  drop.getSource().addFeature(feature);
+}
