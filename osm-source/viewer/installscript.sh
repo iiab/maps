756633cc6105270febe998f4328ee71be4a0b52f
root <root@box.lan>
2020-06-07 13:39:07 +0000

    Modifications

--- osm-source/viewer/installscript.sh
+++ osm-source/viewer/installscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #A simple install script
 
-echo "Getting Webpack Config File"
-cp -f ../generate-regions/pack/webpack.config.js ./webpack.config.js 
+#echo "Getting Webpack Config File"
+#cp -f ../generate-regions/pack/webpack.config.js ./webpack.config.js 
 
-echo "Getting new Assets containing bootstrap-typeahead.min.js and other bootstrap files"
-cp -f ../../generate-regions/ol-parcel-bundler/assets/* ./assets/
+#echo "Getting new Assets containing bootstrap-typeahead.min.js and other bootstrap files"
+#cp -f ../../generate-regions/ol-parcel-bundler/assets/* ./assets/
 
 echo "Install Node"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
--- osm-source/viewer/up.sh
+++ osm-source/viewer/up.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 npm run build
 
-cp -r assets/* ../../webpack/assets/
+
 cp -r assets/* /library/www/osm-vector-maps/viewer/assets/
 
-cp -r build/* ../../webpack/assets/
+
 cp -r build/* /library/www/osm-vector-maps/viewer/
 
-cp -r ./data/geojson/shapequery.geojson /library/www/osm-vector-maps/viewer/data/geojson/
+mkdir -p /library/www/osm-vector-maps/viewer/data/geojson
+
+cp -r ./data/geojson/* /library/www/osm-vector-maps/viewer/data/geojson/
