7112d3d0a799f2a5ad48958f12b0703e3cf35d50
root <root@box.lan>
2020-07-11 15:29:23 +0000

    scripts modified

--- osm-source/viewer/scripts/json2geojson.py
+++ osm-source/viewer/scripts/json2geojson.py
@@ -1,5 +1,7 @@
 from geojson import FeatureCollection, Feature, Point
+import geojson
 import json
+
 
 
 def main():
@@ -9,18 +11,40 @@ def main():
         reg_str = file.read()
         info = json.loads(reg_str)
         for root in info["results"]["bindings"]:
-            print(root["location"]["value"])
-            placeLabel = root["placeLabel"]["value"]
-            loc = root["location"]["value"]
+            lat = float(root["lat"]["value"])
+            long = float(root["long"]["value"])
+            point = Point((long,lat))
+            print(point)
+        #   point = Point((79,43))
+
+            ####################################
+            ##work on getting a dict of properties
+
+            for key in root:
+                print(key)
+
+            ######################################
+
+            features.append(Feature(geometry=point,properties={"name": "test"}))
+
+        collection = FeatureCollection(features)
+        out_str = json.dumps(collection, indent=2, sort_keys=True)
+        print(out_str)
+
+
+    
+    # metrofile = "./metro.geojson"
+    # with open(metrofile,"w") as metro_geojson:
+    #     out_str = json.dumps(collection, indent=2, sort_keys=True)
+    #     metro_geojson.write(outstr)
+        
+        
+        # for root in info["results"]["bindings"]:
+        #     print(root[]["value"])
+        #     placeLabel = root["placeLabel"]["value"]
             
-
-            
-
-                              
-
 
 if __name__ == '__main__':
     main()
 
-print("Hello!") 
 
