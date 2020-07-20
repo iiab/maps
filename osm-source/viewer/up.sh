2d278d1f59e22c86029d6b7d3b140378898e8c53
root <root@box.lan>
2020-07-20 12:00:29 +0000

    Modified

--- osm-source/viewer/up.sh
+++ osm-source/viewer/up.sh
@@ -6,8 +6,6 @@ cp -r build/* /library/www/osm-vector-maps/viewer/
 
 cp jsonserver.php /library/www/osm-vector-maps/viewer/
 
-cp -r data/geojson/* /library/www/osm-vector-maps/viewer/data/geojson/
-
 cp markers/* /library/www/osm-vector-maps/viewer/markers/
 
 rsync -r data/ /library/www/osm-vector-maps/viewer/data
