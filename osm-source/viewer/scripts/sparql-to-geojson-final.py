dbfdf0d33fe86127cf9adaa750a31685e13746a6
root <root@box.lan>
2020-07-25 13:17:23 +0000

    Sample substitution using Python string template

--- osm-source/viewer/scripts/sparql-to-geojson-final.py
+++ osm-source/viewer/scripts/sparql-to-geojson-final.py
@@ -1,5 +1,5 @@
-#Saving SPARQL dump to geojson file
 #!/usr/bin/python3 
+#Saving SPARQL dump to geojson file
 from SPARQLWrapper import SPARQLWrapper, JSON
 from geojson import Feature,Point, FeatureCollection
 from uuid import uuid4
@@ -7,50 +7,58 @@ import geojson
 import json
 import argparse
 import sys
+from string import Template
+#lookup python styles PEP-8
+#camelCase -> underscores
 
 
 SPARQL_ENDPOINT = "https://query.wikidata.org/sparql"
 SPARQL_FROM_TEMPLATE_PATH = "../data/sparql/templates/"
-GEOJSON_PATH = "/library/www/osm-vector-maps/viewer/data/geojson/"
+GEOJSON_PATH = "../data/geojson/"
 
 def main():
     
     parser = argparse.ArgumentParser(description='Get Input and Output File Names')
     parser.add_argument('infile', type=str, help='Input filename for SPARQL Query')
     parser.add_argument('outfile', type=str, help='Output filename for GeoJSON')
-    parser.add_argument('--iconType',default='other',required=False, type=str, help='Output filename for GeoJSON')
+    parser.add_argument('lat', type=float, help='Input latitude for central point for SPARQL Query')
+    parser.add_argument('long', type=float, help='Input latitude for central point for SPARQL Query')
+    parser.add_argument('--featureType',default='other',required=False, type=str, help='Output filename for GeoJSON')
     args = parser.parse_args()
 
 
-    results = get_json_from_sparql(args.infile)
+    results = get_json_from_sparql(args.infile,args.lat,args.long)
     # for result in results["results"]["bindings"]:
     #     print(result["placeDescription"]["value"])
 
-    get_geojson_from_json(results,args.outfile,args.iconType)
+    get_geojson_from_json(results,args.outfile,args.featureType)
     print("Conversion Complete")
     
-def get_json_from_sparql(filename):
+def get_json_from_sparql(filename,lat_value,long_value):
     sparql = SPARQLWrapper(SPARQL_ENDPOINT, agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')
-
-    url = SPARQL_FROM_TEMPLATE_PATH+filename
-    with open(url, 'r') as file:
+    input_file_path = SPARQL_FROM_TEMPLATE_PATH + filename
+    with open(input_file_path, 'r') as file:
         query = file.read().replace('\n', '')
+        query = Template(query).substitute(long = long_value, lat = lat_value)
+        print(query)
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
-        print(results)
         return results
 
-def get_geojson_from_json(results,filename,iconType):
+#featureType dictionary that contains all the possible combinations.
+
+
+def get_geojson_from_json(results,filename,featureType):
     features = []
     info = results
-    iconType = iconType
+    featureType = featureType
     for root in info["results"]["bindings"]:
         lat = float(root["lat"]["value"])
         long = float(root["long"]["value"])
         point = Point((long,lat))
         # print(type(point))
-        properties = {'featureType': iconType}
+        properties = {'featureType': featureType}
         for key in root:
             value = root[key]['value']
             properties[key] = value
--- osm-source/viewer/scripts/test.py
+++ osm-source/viewer/scripts/test.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3 
+import argparse
+from string import Template
+parser = argparse.ArgumentParser(description='Get Input and Output File Names')
+parser.add_argument('lat', type=float, help='Input latitude for central point for SPARQL Query')
+parser.add_argument('long', type=float, help='Input latitude for central point for SPARQL Query')
+args = parser.parse_args()
+point = "Hello Point({long} {lat})"
+point = point.format(long = args.long, lat = args.lat)
+query = Template('''
 SELECT ?place ?placeLabel ?image ?location ?dist ?lat ?long ?type WHERE {
   wd:Q2341950 wdt:P625 ?loc.
   SERVICE wikibase:around {
@@ -16,3 +26,10 @@ SELECT ?place ?placeLabel ?image ?location ?dist ?lat ?long ?type WHERE {
   BIND(STRBEFORE(STRAFTER(STR(?location), " "), ")") AS ?lat)
   BIND(STRBEFORE(STRAFTER(STR(?location), "Point("), " ") AS ?long)
 }
+ ''').substitute(long = args.long, lat = args.lat)
+#query = query.format(long = args.long, lat = args.lat)
+print(query)
+   
+
+
+
