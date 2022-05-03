51a704c934053803d3691c62f32985aeb6635075
Jerry Vonau <jvonau3@gmail.com>
2022-05-02 20:46:34 -0500

    iiab-make-init.py - full pylint

--- osm-source/pages/viewer/scripts/iiab-make-init.py
+++ osm-source/pages/viewer/scripts/iiab-make-init.py
@@ -1,64 +1,53 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-
-import iiab.iiab_lib as iiab
-
-
+"""Writes init.json """
+import sys
 import argparse
-import sys, os
-import json
-import glob
-import shutil
 import json
 
 # GLOBALS
-viewer_path = '/library/www/osm-vector-maps/viewer'
-catalog_path = '/etc/iiab'
-map_catalog = {}
+VIEWER_PATH = '/library/www/osm-vector-maps/viewer'
+CATALOG_PATH = '/etc/iiab/map-catalog.json'
+MAP_CATALOG = {}
 
 if len(sys.argv) != 2:
     print("Argument 1=map_url")
     sys.exit(1)
 
 def get_map_catalog():
-    global map_catalog
-    input_json = '/etc/iiab/map-catalog.json'
+    """returns CATALOG_PATH entries"""
+    input_json = CATALOG_PATH
     with open(input_json, 'r') as regions:
         reg_str = regions.read()
         map_catalog = json.loads(reg_str)
     #print(json.dumps(map_catalog, indent=2))
     return map_catalog
 
-def subproc_cmd(cmdstr, shell=False):
-    args = shlex.split(cmdstr)
-    outp = subproc_check_output(args, shell=shell)
-    return (outp)
-
 def parse_args():
+    """returns parsed args"""
     parser = argparse.ArgumentParser(description="Create init.json for a tile URL.")
     parser.add_argument("map_url", help="The 'detail_url' field in mapcatalog.json.")
     return parser.parse_args()
 
 def main():
-    global map_catalog
+    """create init.json which sets initial coords and zoom"""
     args = parse_args()
     map_catalog = get_map_catalog()
     catalog = map_catalog['maps']
     #for k in catalog.keys():
       #print(k)
-    map   = catalog.get(args.map_url,{})
-    if  len(map) == 0:
+    map2 = catalog.get(args.map_url,{})
+    if  len(map2) == 0:
         print('Download URL not found in map-catalog.json: %s'%args.map_url)
         sys.exit(1)
 
-    # create init.json which sets initial coords and zoom
     init = {}
-    map = catalog[args.map_url]
-    init['region'] = map['region']
-    init['zoom'] = map['zoom']
-    init['center_lon'] = map['center_lon']
-    init['center_lat'] = map['center_lat']
-    init_fn = viewer_path + '/init.json'
+    map3 = catalog[args.map_url]
+    init['region'] = map3['region']
+    init['zoom'] = map3['zoom']
+    init['center_lon'] = map3['center_lon']
+    init['center_lat'] = map3['center_lat']
+    init_fn = VIEWER_PATH + '/init.json'
     with open(init_fn,'w') as init_fp:
         init_fp.write(json.dumps(init,indent=2))
 
